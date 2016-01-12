c229ed6a8c078b320167fe17deeb3c0bcdb050f7
zanjs <root@zanjs.com>
2016-01-12 20:32:51 +0800

    :cat:

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -7,14 +7,14 @@ class Contact extends React.Component {
             <div>
                 <div className="lets-talk">
                     <h3>Lets Talk</h3>
-                    <a href="https://twitter.com/DimitriMikadze" target="_blank">Twitter →</a>
+                    <a href="https://twitter.com/zanjs" target="_blank">Twitter →</a>
                     <a href="https://www.linkedin.com/profile/view?id=AAMAABSP054BMC3w5ePc2YYXGPRJsMWdAG3H6Fs&trk=hp-identity-name" target="_blank">Linkedin →</a>
                     <a href="https://www.facebook.com/Ditmens" target="_blank">Facebook →</a>
                 </div>
 
                 <div className="github">
                     <h3>Find me on Github</h3>
-                    <a href="https://github.com/DimitriMikadze" target="_blank">
+                    <a href="https://github.com/zanjs" target="_blank">
                         Github →
                     </a>
                 </div>
